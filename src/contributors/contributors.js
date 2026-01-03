f89eebf592e3b3ff3b85962ea1e958ddd54e9528
lockedmutex <127536688+lockedmutex@users.noreply.github.com>
2026-01-03 01:23:37 +0000

    chore: update contributors from Codeberg

--- src/contributors/contributors.js
+++ src/contributors/contributors.js
@@ -3,7 +3,7 @@ window.SAMAYA_CONTRIBUTORS = [
         "username": "lockedmutex",
         "avatar_url": "https://codeberg.org/avatars/45e02b4a39a053f0bc185fd02c54ef10aac8667ce27e64195e3992db7bfd8cca",
         "profile_url": "https://codeberg.org/lockedmutex",
-        "contributions": 21,
+        "contributions": 22,
         "local_image": "./contributors/images/lockedmutex.avif"
     },
     {
@@ -17,14 +17,21 @@ window.SAMAYA_CONTRIBUTORS = [
         "username": "hthienloc",
         "avatar_url": "https://codeberg.org/avatars/ba69859398ae016ed05d9f0474455d19",
         "profile_url": "https://codeberg.org/hthienloc",
-        "contributions": 3,
+        "contributions": 2,
         "local_image": "./contributors/images/hthienloc.avif"
+    },
+    {
+        "username": "Vistaus",
+        "avatar_url": "https://codeberg.org/avatars/fc8e4e41ba93a3067f0370f1bd7c600a",
+        "profile_url": "https://codeberg.org/Vistaus",
+        "contributions": 1,
+        "local_image": "./contributors/images/vistaus.avif"
     },
     {
         "username": "ma12vlad",
         "avatar_url": "https://codeberg.org/avatars/86497939d347b96a028635c5a588f9ee8aa6c14700739c31c7ca21c3235fd6ff",
         "profile_url": "https://codeberg.org/ma12vlad",
-        "contributions": 2,
+        "contributions": 1,
         "local_image": "./contributors/images/ma12vlad.avif"
     },
     {
